5cf33445082a1ca4695316df926cba474e2a52f7
Ronald Wildenberg <rwwilden@gmail.com>
2019-03-06 14:13:06 +0100

    Added ErrorResponse and Error classes for deserializing Azure RM and Azure Graph errors

---
+++ src/azure/Errors/Error.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace azure.Errors
+{
+    [JsonObject]
+    public class Error
+    {
+        [JsonProperty]
+        public string Code { get; private set; }
+
+        [JsonProperty]
+        public string Message { get; private set; }
+    }
+}
